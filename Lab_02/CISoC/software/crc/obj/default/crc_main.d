ec7a74b53fa6e6cf018f0c8e1d1923234f3f587d
mathisha034 <150885341+mathisha034@users.noreply.github.com>
2026-01-15 14:27:52 +0530

    Adding Lab 02

---
+++ Lab_02/CISoC/software/crc/obj/default/crc_main.d
@@ -0,0 +1,29 @@
+obj/default/crc_main.o: crc_main.c ../crc_bsp/system.h \
+ ../crc_bsp/linker.h crc.h ci_crc.h \
+ ../crc_bsp//HAL/inc/sys/alt_timestamp.h ../crc_bsp//HAL/inc/alt_types.h \
+ ../crc_bsp//drivers/inc/altera_avalon_timer.h \
+ ../crc_bsp//HAL/inc/sys/alt_dev.h ../crc_bsp//HAL/inc/sys/alt_llist.h \
+ ../crc_bsp//HAL/inc/priv/alt_dev_llist.h \
+ ../crc_bsp//HAL/inc/sys/alt_warning.h
+
+../crc_bsp/system.h:
+
+../crc_bsp/linker.h:
+
+crc.h:
+
+ci_crc.h:
+
+../crc_bsp//HAL/inc/sys/alt_timestamp.h:
+
+../crc_bsp//HAL/inc/alt_types.h:
+
+../crc_bsp//drivers/inc/altera_avalon_timer.h:
+
+../crc_bsp//HAL/inc/sys/alt_dev.h:
+
+../crc_bsp//HAL/inc/sys/alt_llist.h:
+
+../crc_bsp//HAL/inc/priv/alt_dev_llist.h:
+
+../crc_bsp//HAL/inc/sys/alt_warning.h:
